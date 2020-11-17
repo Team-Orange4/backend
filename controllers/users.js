b5b2f68d424aa42f8b80b65057638cb1b8720833
d-e-k-k <davideberhart8@gmail.com>
2020-11-16 22:19:34 -0500

    Created users controller and show all get route

---
+++ controllers/users.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/', (req, res, next) => {
+	User.find({})
+		.then((users) => {
+			res.json(users);
+		})
+		.catch(next);
+});
+
+module.exports = router;
