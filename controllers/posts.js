ad2d315eea2914553bf38bf69c2b293d590de6ba
datacreator001 <amirrorphone@gmail.com>
2020-11-17 14:46:45 -0500

    Added Post route for post

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,6 +9,11 @@ router.get('/', (req, res, next) => {
 		.then((posts) => res.json(posts))
 		.catch(next);
 });
-
+ router.post('/',(req,res,next)=>{
+     const postData=req.body
+     Posts.create(postData)
+     .then((post)=>res.status(201).json(post))
+     .catch(next)
+ })
 
 module.exports=router
