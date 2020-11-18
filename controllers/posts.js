b5cae073270522868e9300233ba45ebfbc72110b
datacreator001 <amirrorphone@gmail.com>
2020-11-18 12:26:30 -0500

    added all the routes and postseed.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,12 +9,32 @@ router.get('/', (req, res, next) => {
 		.then((posts) => res.json(posts))
 		.catch(next);
 });
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id;
+	Posts.findById(id)
+		.then((post) => res.json(post))
+		.catch(next);
+});
  router.post('/',(req,res,next)=>{
      const postData=req.body
      Posts.create(postData)
      .then((post)=>res.status(201).json(post))
      .catch(next)
  })
+ router.patch('/:id', (req, res, next) => {
+		const id = req.params.id;
+		const postData = req.body;
+		Posts.findOneAndUpdate({ _id: id }, postData, { new: true })
+			.then((post) => res.json(post))
+			.catch(next);
+ });
+
  
+ router.delete('/:id', (req, res, next) => {
+		const id = req.params.id;
+		Posts.findOneAndDelete({ _id: id })
+			.then(() => res.sendStatus(204))
+			.catch(next)})
+ ;
 
 module.exports=router
