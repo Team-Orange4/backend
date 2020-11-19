4ba50e82e06d2a8bb396fec2be12164295f95e8f
d-e-k-k <davideberhart8@gmail.com>
2020-11-19 08:03:00 -0500

    Cleaned up package.json.lock issues from rebase

--- controllers/users.js
+++ controllers/users.js
@@ -5,51 +5,22 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { get } = require('mongoose');
 
 app = express();
 app.use(express.json());
 
-<<<<<<< HEAD
-//SHOW all users ****THIS ROUTE IS JUST FOR DEV. WILL NOT BE IN PRODUCTION
-router.get('/', (req, res, next) => {
-	// User.find({})
-	// 	.then(users => {
-	// 		users.filter(user => user.body.email === req.user.email)
-	// 	})
-	// 	.catch(next)
-	// })
-=======
 router.get('/', authToken, (req, res, next) => {
->>>>>>> Auth added for GET returning only logged in user info
 	User.find({})
-		.then(users => {
-			return users.find(user => user.email === req.user.email)
-			// console.log(users[0].email);
-			// console.log(req.user.email);
-			// users.find(user => user.email === req.body.email)
+		.then((users) => {
+			return users.find((user) => user.email === req.user.email);
 		})
-		.then(user => {
-			console.log(user)
-			res.json(user)
+		.then((user) => {
+			console.log(user);
+			res.json(user);
 		})
-		// .then(user => {
-		// 	console.log(user);
-		// 	if(user){
-		// 		res.json(user)
-		// 	}else{
-		// 		res.status(401).json('Please log in to view this page')
-		// 	}
-		// })
-		.catch(next)
-	})
-	
-	// User.find({})
-	// 	.then((users) => {
-	// 		res.json(users);
-	// 	})
-	// 	.catch(next);
-	// })
-
+		.catch(next);
+});
 
 //this might be at address of .../register
 router.post('/register', async (req, res, next) => {
@@ -94,15 +65,26 @@ router.post('/login', (req, res, next) => {
 
 function authToken(req, res, next) {
 	const authHeader = req.headers['authorization'];
-	const token = authHeader && authHeader.split(" ")[1];
-	if(token == null) return res.send("no token")
+	const token = authHeader && authHeader.split(' ')[1];
+	if (token == null) return res.send('no token');
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if(err) return res.send("no access but has token")
-		req.user = user
-		next()
-	})
+		if (err) return res.send('no access but has token');
+		req.user = user;
+		next();
+	});
 }
+
+
+///DEV ONLY ROUTES DELETE BEFORE DEPLOYMENT **START**
+router.get('/all', (req, res, next) => {
+	User.find({})
+		.then(users => res.json(users))
+		.catch(next)
+})
+
+///DEV ONLY ROUTES DELETE BEFORE DEPLOYMENT **END**
+
 
 module.exports = router;
 
